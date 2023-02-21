3c4506b69a4f30b52ba440c1fd04e04bdcc05f35
Lucas Cordeiro <lucas.cordeiro@smarttbotter.com>
2023-02-21 11:59:53 -0300

    feat: add redirect in header components

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,14 +3,15 @@ import { DefaultLayout } from "./layouts/DefaultLayout";
 import { Checkout } from "./Pages/Checkout";
 import { Home } from "./Pages/Home ";
 import { Success } from "./Pages/Success";
+import { ROUTES } from "./utils/constants";
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/success" element={<Success />} />
+      <Route path={ROUTES.home} element={<DefaultLayout />}>
+        <Route path={ROUTES.home} element={<Home />} />
+        <Route path={ROUTES.checkout} element={<Checkout />} />
+        <Route path={ROUTES.success} element={<Success />} />
       </Route>
     </Routes>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,20 +2,26 @@ import { Cart, Counter, HeaderContainer, Location } from "./styles";
 import { ShoppingCart, MapPin } from "phosphor-react";
 
 import logo from "../../assets/images/logo.svg";
+import { Link } from "react-router-dom";
+import { ROUTES } from "../../utils/constants";
 
 export const Header = () => {
   return (
     <HeaderContainer>
-      <img src={logo} alt="Coffee delivery" />
+      <Link to={ROUTES.home}>
+        <img src={logo} alt="Coffee delivery" />
+      </Link>
       <div>
         <Location>
           <MapPin weight="fill" size={22} />
           <span>Congonhas, MG</span>
         </Location>
-        <Cart>
-          <ShoppingCart weight="fill" size={22} />
-          <Counter>3</Counter>
-        </Cart>
+        <Link to={ROUTES.checkout}>
+          <Cart>
+            <ShoppingCart weight="fill" size={22} />
+            <Counter>3</Counter>
+          </Cart>
+        </Link>
       </div>
     </HeaderContainer>
   );
