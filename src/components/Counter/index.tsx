c68ceaa257d5af22a6f0b07e1616e254156dfbe0
Lucas Cordeiro <lucas.cordeiro@smarttbotter.com>
2023-02-23 21:04:01 -0300

    feat: add min, max value and initial value to the counter component

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,18 +1,30 @@
 import { useState } from "react";
 import { CounterContainer } from "./styles";
 
+const MIN_VALUE = 0;
+const MAX_VALUE = 99;
+
 type CounterProps = {
   onChangeValue: (value: number) => void;
   disabled?: boolean;
+  initialValue?: number;
+  minValue?: number;
+  maxValue?: number;
 };
 
-export const Counter = ({ onChangeValue, disabled = false }: CounterProps) => {
-  const [value, setValue] = useState(0);
+export const Counter = ({
+  onChangeValue,
+  disabled = false,
+  initialValue = 0,
+  minValue = MIN_VALUE,
+  maxValue = MAX_VALUE,
+}: CounterProps) => {
+  const [value, setValue] = useState(initialValue);
 
   const handleSumClick = () => {
     const newValue = value + 1;
 
-    if (value >= 99) return;
+    if (value >= maxValue) return;
 
     setValue(newValue);
     onChangeValue(newValue);
@@ -21,7 +33,7 @@ export const Counter = ({ onChangeValue, disabled = false }: CounterProps) => {
   const handleSubtractionClick = () => {
     const newValue = value - 1;
 
-    if (value <= 0) return;
+    if (value <= minValue) return;
 
     setValue(newValue);
     onChangeValue(newValue);
